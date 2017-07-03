7e5ab6cc9a28f7bb9018db9f2709be2777fd92e0
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-07-03 22:57:20 +0200

    Fix tests

--- aurelia-ux-todo/test/unit/app.spec.ts
+++ aurelia-ux-todo/test/unit/app.spec.ts
@@ -1,7 +1,19 @@
 import {App} from '../../src/app';
 
+class UX {
+    public design = {
+        primary: '',
+        accent: '',
+    };
+}
+
+
 describe('the app', () => {
-  it('says hello', () => {
-    expect(new App().message).toBe('Hello World!');
-  });
+    it('says hello', () => {
+        let ux = new UX();
+        let sut = new App(ux);
+        expect(ux.design.primary).toBe('#009688');
+        expect(ux.design.accent).toBe('#4CAF50');
+        expect(sut.showNavigationMenu).toBe(false);
+    });
 });
