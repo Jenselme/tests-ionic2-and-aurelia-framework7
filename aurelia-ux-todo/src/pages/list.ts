0384c629eac4c5b8b06b559174ec004fd951b254
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-07-06 23:29:31 +0200

    Can list TODOs

--- aurelia-ux-todo/src/pages/list.ts
+++ aurelia-ux-todo/src/pages/list.ts
@@ -1,3 +1,34 @@
-export class Home {
+import { Todo } from '../models/todo';
+import { Storage } from '../services/storage';
+import { autoinject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
 
+
+@autoinject
+export class List {
+    public todos: Todo[];
+
+    constructor(private storage: Storage, private router: Router) {
+        this.todos = this.storage.getTodos();
+    }
+
+    public viewTodo(todo: Todo) {
+        this.router.navigateToRoute('todo', {id: todo.id});
+    }
+
+    public addTodo() {
+        this.router.navigate('/add');
+    }
+
+    toggleDone(todo: Todo, event: Event) {
+        event.stopPropagation();
+
+        if (todo.done) {
+            todo.done = false;
+        } else {
+            todo.done = true;
+        }
+
+        this.storage.saveTodo(todo);
+    }
 }
