6dce85ba1d709e1b3c7717dfbcd50d597cf7d978
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-07-07 00:00:14 +0200

    Rename add page into edit
    Will be used to add and edit TODOs

--- aurelia-ux-todo/src/routes.ts
+++ aurelia-ux-todo/src/routes.ts
@@ -7,5 +7,5 @@ export let routes = [
     { route: '', redirect: 'list' },
     { settings: { category: todoCategory }, route: 'list', moduleId: './pages/list', name: 'list', title: 'TODO list',  nav: true },
 
-    { settings: { category: todoCategory }, route: 'add', moduleId: './pages/add', name: 'add', title: 'Add todo', nav: true },
+    { settings: { category: todoCategory }, route: 'add', moduleId: './pages/edit', name: 'add', title: 'Add todo', nav: true },
 ];
