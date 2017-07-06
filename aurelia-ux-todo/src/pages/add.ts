78c4f5e73845d12820fe2bb1e0c8e1e8b6c63f40
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-07-06 23:04:33 +0200

    Can add TODOS

--- aurelia-ux-todo/src/pages/add.ts
+++ aurelia-ux-todo/src/pages/add.ts
@@ -1,6 +1,31 @@
+import { autoinject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+
+import { Todo } from '../models/todo';
+import { Storage } from '../services/storage';
+
+
+@autoinject
 export class Add {
-    id: number;
-    title: string;
-    description: string;
-    done: boolean;
+    public todo: Todo;
+
+    constructor(private router: Router, private storage: Storage) {
+    }
+
+    public activate(params) {
+        if (params.id) {
+            this.todo = this.storage.getTodo(params.id);
+        } else {
+            this.todo = new Todo();
+        }
+    }
+
+    public save() {
+        this.storage.saveTodo(this.todo);
+        this.router.navigateToRoute('list');
+    }
+
+    public cancel() {
+        this.router.navigateToRoute('list');
+    }
 }
